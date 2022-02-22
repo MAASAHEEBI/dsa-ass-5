c0d7242302a9df186e9de2f067a304d2161622f8
MAASAHEBBI USTAD <mhsrmy1@gmail.com>
2022-02-22 13:44:29 +0530

    dsa ass 5

---
+++ dsa5.py
@@ -0,0 +1,14 @@
+#Q5. Read about the Tower of Hanoi algorithm. Write a program to implement it.
+
+
+
+def TowerOfHanoi(n , from_rod, to_rod, aux_rod):
+    if n == 0:
+        return
+    TowerOfHanoi(n-1, from_rod, aux_rod, to_rod)
+    print("Move disk",n,"from rod",from_rod,"to rod",to_rod)
+    TowerOfHanoi(n-1, aux_rod, to_rod, from_rod)
+         
+# Driver code
+n = 4
+TowerOfHanoi(n, 'A', 'C', 'B')
